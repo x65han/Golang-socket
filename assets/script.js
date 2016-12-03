031e4c764d730697056c8a12c5c141db3e929fd0
x65han <x65han@uwaterloo.ca>
2016-12-02 23:59:09 -0500

    key stroke detection

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,37 @@
 window.onload = function(){
-	alert("script.js is working");
-	socket = io.connect("");
-	socket.emit("ping", "Johnson")
-	socket.on('pong', function(data){
-			 alert("server pong " + data);
-	 });
+		// Key Stroke
+		document.body.onkeydown = function(e) {
+				var ev = e || event;var key = ev.keyCode;
+				if(ev.keyCode == 13) {
+						sendMessage();
+				}else if(key == 16){
+						socket.emit("status", "");
+				}
+		}
+		// Establish TCP Connection
+		establishConnection();
+}
+function establishConnection(){
+		// Connect to Socket.io
+		socket = io.connect("");
+		// Socket Receiver
+		socket.on('pong', function(data){
+				 console.log("pong: " + data);
+		});
+		socket.on('pipe', function(data){
+				 console.log("pipe: >>");
+				 console.log(data);
+		});
+}
+function sendMessage(){
+		var message = document.getElementById("message-box").value.trim();
+		socket.emit("ping", message);
+		document.getElementById("message-box").value = "";
+		console.log("ping: " + message);
+}
+function connectToRoom(){
+		var roomName = document.getElementById("message-box").value.trim();
+		socket.emit("connect to room", roomName)
+		document.getElementById("message-box").value = "";
+		console.log("Connect to: " + roomName);
 }
