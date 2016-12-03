b1912615bb689cfa74135735b1a369572e1d7462
x65han <x65han@uwaterloo.ca>
2016-12-02 23:58:39 -0500

    socket user management

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 	"os"
 	"fmt"
-	"time"
+	"strconv"
 	"github.com/gin-gonic/gin"
 	"github.com/googollee/go-socket.io"
 )
@@ -31,6 +31,9 @@ func main() {
 
 	// Socket.io Setup
 	Socketio_Server, _ = socketio.NewServer(nil)
+	rooms = make(map[socketio.Socket]string)
+	names = make(map[socketio.Socket]string)
+	sockets = make([]socketio.Socket,0)
 	router.GET  ("/socket.io/", socketHandler)
 	router.POST ("/socket.io/", socketHandler)
 	router.Handle ("WS",  "/socket.io/", socketHandler)
@@ -41,21 +44,45 @@ func main() {
 	router.Run(":" + port)
 }
 
+var sockets []socketio.Socket
+var rooms map[socketio.Socket]string
+var names map[socketio.Socket]string
+
 func socketHandler (c  *gin.Context) {
     Socketio_Server.On("connection", func(socket socketio.Socket) {
-        fmt.Println("on connection")
-        socket.Join("galaxy")
+				// Initialize Socket
+				sockets = append(sockets, socket)
+				rooms[socket] = "galaxy"
+				names[socket] = "wuffle"
+				socket.Join(rooms[socket])
+				// Socket Receiver
 				socket.On("ping", func(msg string){
-						fmt.Println(msg, " pinging")
-						msg = time.Now().Format(time.RFC850)
-						socket.BroadcastTo("galaxy", "pong", msg)
+						Socketio_Server.BroadcastTo(rooms[socket], "pong", msg)
 				})
-        socket.On("chat message", func(msg string) {
-            fmt.Println("emit:", socket.Emit("chat message", msg))
-            socket.BroadcastTo("galaxy", "chat message", msg)
-        })
+				socket.On("status", func(msg string){
+						res := "\nCurrent room: " + rooms[socket]
+						res = res + "\n # of sockets: " + strconv.Itoa(len(sockets))
+						res = res + "\n # of rooms: " + strconv.Itoa(len(rooms))
+						res = res + "\n # of names: " + strconv.Itoa(len(names))
+						socket.Emit("pipe", res)
+				})
+				socket.On("connect to room", func(roomName string){
+						socket.Leave(rooms[socket])
+						rooms[socket] = roomName
+						socket.Join(rooms[socket])
+						fmt.Println("connec to room: ", rooms[socket])
+				})
         socket.On("disconnection", func() {
-            fmt.Println("on disconnect")
+						fmt.Println(names[socket], " left ", rooms[socket])
+						socket.Leave(rooms[socket])
+						delete(rooms, socket)
+						delete(names, socket)
+						for index, x := range sockets{
+								if(x == socket){
+										sockets = append(sockets[:index], sockets[index+1:]...)
+										break;
+								}
+						}
         })
     })
     Socketio_Server.On ( "error", func(socket socketio.Socket, err error) {
