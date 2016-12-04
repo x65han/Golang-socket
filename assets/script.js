2596a4e3f14234e030bf6b75aefe2a1d89d5eed5
x65han <x65han@uwaterloo.ca>
2016-12-04 10:40:56 -0500

    Extra control button for play and pause

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ window.onload = function(){
 		// Establish TCP Connection
 		establishConnection();
 		$('#roomname').css("background-color", "crimson");
+		$('#roomname').css("color", "white");
 }
 function establishConnection(){
 		// Connect to Socket.io
@@ -22,6 +23,15 @@ function establishConnection(){
 		socket.on('pipe', function(data){
 				 console.log("pipe: >>");
 				 console.log(data);
+		});
+		socket.on('play', function(data){
+				if(playerControl.cpu != null){
+						playerControl.cpu.target.playVideo();
+						var duration = playerControl.cpu.target.getDuration();
+						console.log("Total time: " + duration);
+				}else{
+						alert("player control cpu error");
+				}
 		});
 }
 function sendMessage(){
@@ -41,4 +51,21 @@ function updateUsername(){
 		socket.emit("update username", username)
 		document.getElementById("message-box").value = "";
 		console.log("Update username: " + username);
+}
+function seekTo(time){
+	if(playerControl.cpu != null){
+			playerControl.cpu.target.seekTo(time);
+	}else{
+			alert("player control cpu error");
+	}
+}
+function requestToPlay(){
+		socket.emit("request to play", playerControl.videoId);
+}
+function requestToStop(){
+		if(playerControl.cpu != null){
+				playerControl.cpu.target.pauseVideo()
+		}else{
+				alert("player control cpu error");
+		}
 }
